b31c537f0194206e841881ce7e7241f1a70dc807
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2026-03-05 20:23:05 +1000

    fix: Remove serde constraints

--- src/serialization.rs
+++ src/serialization.rs
@@ -1,13 +1,13 @@
 pub fn bincode_encode_to_vec<T>(value: &T) -> crate::db_type::Result<Vec<u8>>
 where
-    T: serde::Serialize + native_model::Model,
+    T: native_model::Model,
 {
     native_model::encode(value).map_err(|e| e.into())
 }
 
 pub fn bincode_decode_from_slice<T>(slice: &[u8]) -> crate::db_type::Result<(T, usize)>
 where
-    T: serde::de::DeserializeOwned + native_model::Model,
+    T: native_model::Model,
 {
     let (data, _) = native_model::decode(slice.to_vec())?;
     Ok((data, 0))
